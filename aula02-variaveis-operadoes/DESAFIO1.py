14626c0dd163bd22d66f82195267b5f0a800cf55
28e8hrhr <tcarloseduardo802@gmail.com>
2026-03-16 11:40:38 -0300

    aula 2 variavel

---
+++ aula02-variaveis-operadoes/DESAFIO1.py
@@ -0,0 +1,2 @@
+nome = input("DIGITE SEU NOME")
+print(f"boas vindas {nome}")
---
+++ aula02-variaveis-operadoes/aula02a-variaveis.py
@@ -0,0 +1,19 @@
+print('Olá, mundo!')
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # ESTOU CONCARTENANDO SRINGS
+
+# São
+
+
+# VARIÁVÉIS
+#a variavel nome recebe 'ALEXANDRE E GUARDA o nome
+nome = "alexandre"
+idade =26
+peso=70,2
+print(nome, idade, peso)
+print(f*0111,  {nome})
+
+#INPUT -- FORMULARIO SIMPLES NO PROMPT DE COMANDO(CMD)
+nome = input("DIGITE SEU NOME")
